f7a1d9dbb8957fb74aaab5138dc43766a4288cca
unknown <ithaqua16@gmail.com>
2019-03-13 09:42:20 +0100

    save

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -26,7 +27,7 @@ class UserController extends Controller
         $user->email = request('email');
 
         if (request('password') != "") {
-            $user->password = request('password');
+            $user->password = Hash::make(request('password'));
         }
 
         $user->save();
